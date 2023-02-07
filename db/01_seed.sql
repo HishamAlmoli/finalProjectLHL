1c68139112bd5e5f3a1c09eb9b59dd822b0f9a62
Brandon <bbeam1@gmail.com>
2023-02-06 21:49:23 -0700

    database setup

---
+++ db/01_seed.sql
@@ -0,0 +1,34 @@
+INSERT INTO parents (name, email, phone, password)
+VALUES ('John Doe', 'johndoe@gmail.com', '(111)111-1111', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+
+INSERT INTO activities (name, description, out_of_daycare)
+VALUES ('eating', 'eating time', FALSE), 
+('painting', 'painting time', FALSE),
+('drawing', 'drawing time', FALSE),
+('play', 'play time', FALSE),
+('nap', 'nap time', FALSE),
+('zoo', 'trip to the zoo', TRUE);
+
+
+INSERT INTO employees (name, phone, password)
+VALUES ('John Smith','(111)111-1112', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+
+INSERT INTO schedules (activity_id, date_time)
+VALUES (1, '2023-02-06 9:00:00'),
+(5, '2023-02-06 10:00:00'),
+(4, '2023-02-06 11:00:00'),
+(3, '2023-02-06 12:00:00'),
+(2, '2023-02-06 13:00:00'),
+(1, '2023-02-06 14:00:00');
+
+
+INSERT INTO children (age_group, parent_id, name, notes, birthday)
+VALUES ('toddler', 1, 'tim', 'tiny', '2019-02-06');
+
+INSERT INTO employees_children(employee_id, child_id)
+VALUES (1, 1);
+
+INSERT INTO schedules_children(schedule_id, child_id)
+VALUES (1, 1);
---
+++ db/schema.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS parents CASCADE;
+DROP TABLE IF EXISTS activities CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS schedules CASCADE;
+DROP TABLE IF EXISTS children CASCADE;
+DROP TABLE IF EXISTS employees_children CASCADE;
+DROP TABLE IF EXISTS schedules_children CASCADE;
+--parents could alternatively be account holder
+CREATE TABLE parents (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(15) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE activities (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  out_of_daycare BOOLEAN NOT NULL DEFAULT FALSE
+);
+--schedule items might be better name
+CREATE TABLE schedules (
+  id SERIAL PRIMARY KEY NOT NULL,
+  activity_id INTEGER REFERENCES activities(id) ON DELETE CASCADE,
+  date_time TIMESTAMP
+);
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(15) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE children (
+  id SERIAL PRIMARY KEY NOT NULL,
+  parent_id INTEGER REFERENCES parents(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  notes TEXT,
+  birthday DATE,
+  age_group TEXT
+);
+
+CREATE TABLE employees_children (
+id SERIAL PRIMARY KEY NOT NULL,
+employee_id INTEGER REFERENCES employees(id) ON DELETE CASCADE,
+child_id INTEGER REFERENCES children(id) ON DELETE CASCADE
+);
+
+CREATE TABLE schedules_children (
+ id SERIAL PRIMARY KEY NOT NULL,
+ child_id INTEGER REFERENCES children(id) ON DELETE CASCADE,
+ schedule_id INTEGER REFERENCES schedules(id) ON DELETE CASCADE
+);
+/*
+if we want to allow multiple accounts to access a child we can use this
+CREATE TABLE parents_children (
+id SERIAL PRIMARY KEY NOT NULL,
+parent_id INTEGER REFERENCES parents(id) ON DELETE CASCADE,
+child_id INTEGER REFERENCES children(id) ON DELETE CASCADE,
+);
+*/
